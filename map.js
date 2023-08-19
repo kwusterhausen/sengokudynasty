7fb61b8e456a0bcb14c6eae2844d85ef2f3af322
kwusterhausen <kwusterhausen@googlemail.com>
2023-08-20 00:17:33 +0200

    Update map.js

--- map.js
+++ map.js
@@ -2,7 +2,8 @@ const map = document.getElementById('map');
 const legendIcons = document.querySelectorAll('.legend button');
 const iconPositions = [
     [{ x: 880, y: 377 }, { x: 300, y: 100 }], // Koordinaten für "iron.png"
-    [{ x: 400, y: 400 }] // Koordinaten für "clay.png"
+    [{ x: 343, y: 330 }, { x: 350, y: 328 }], // Koordinaten für "clay.png"
+    [{ x: 470, y: 383 }, { x: 470, y: 390 }, { x: 480, y: 383 }, { x: 480, y: 390 }], // Koordinaten für "grass.png"
     // Weitere Koordinaten für andere Bilder können hier hinzugefügt werden
 ];
 
